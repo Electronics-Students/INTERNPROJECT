56bbd171c83740f6909944157e89e0fa415363d0
Electronics-Students <electronics.edmunds.students@gmail.com>
2025-07-03 17:32:54 +0530

    Add files via upload

---
+++ real_time_tempandhumidity_logger_arduinoidecode.ino
@@ -0,0 +1,55 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "DHT.h"
+
+#define DHTPIN 4          // DHT11 data pin connected to GPIO 4
+#define DHTTYPE DHT11     // DHT11 Sensor
+
+const char* ssid = "redmi125g";
+const char* password = "1234567890";
+
+// Google Script Web App URL
+const char* serverName = "enter the google script id";
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected");
+}
+
+void loop() {
+  if(WiFi.status() == WL_CONNECTED){
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+
+    if (isnan(temperature) || isnan(humidity)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+
+    String jsonData = "{\"temperature\":\"" + String(temperature) + "\",\"humidity\":\"" + String(humidity) + "\"}";
+    int httpResponseCode = http.POST(jsonData);
+
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(120000); // wait for 1 minutes (120000 ms)
+}
